1bd4c1dece62545a52cc9d25c32bad6412448fad
seObando19 <sebastiano-10@hotmail.com>
2020-09-03 19:23:28 +0000

    Updated v3  task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import hidden_4
-data = dir(hidden_4)
-for i in data:
-    if i[0] != '__':
-        print(i)
+    import hidden_4
+    data = dir(hidden_4)
+    for i in data:
+        if i[0] != '__':
+            print(i)
