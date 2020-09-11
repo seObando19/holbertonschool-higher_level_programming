ae17928750b5df0ebf3b169877e2f32174e155e2
seObando19 <sebastiano-10@hotmail.com>
2020-09-11 01:12:44 +0000

    Updated task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     mydict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
-    if type(roman_string) != str and roman_string is None:
+    if type(roman_string) != str or roman_string is None:
         return 0
     for i in range(len(roman_string)):
         if i > 0 and mydict[roman_string[i]] > mydict[roman_string[i - 1]]:
