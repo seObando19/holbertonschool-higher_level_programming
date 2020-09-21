1cfd09895afaa05753c2f8834f5a05e7964de176
seObando19 <sebastiano-10@hotmail.com>
2020-09-21 19:41:56 +0000

    Completed task 0 and add test 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        while i < x:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+        print("")
+        return i
+    except:
+        print("")
+        return i
