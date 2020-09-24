0a1c8365cb4a8bd637458236f8e5872276a87795
seObando19 <sebastiano-10@hotmail.com>
2020-09-24 01:44:15 +0000

    Updated v6 of task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,11 +8,11 @@ class Square:
     """init Square and raising error"""
     def __init__(self, size=0):
         self.size = size
-	'''getter size'''
+    """getter size"""
     @property
     def size(self):
         return self.__size
-    '''setter size'''
+    """setter size"""
     @size.setter
     def size(self, size):
         if type(size) is not int:
