2121e45e552ea947f8e8d947e4b6430d4ef711d6
seObando19 <sebastiano-10@hotmail.com>
2020-09-24 02:02:45 +0000

    Completed task 5 and added test 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square.
+"""
+
+
+class Square:
+    """init Square and raising error"""
+    def __init__(self, size=0):
+        self.size = size
+    """getter size"""
+    @property
+    def size(self):
+        return self.__size
+    """setter size"""
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            lenght = self.__size
+            for i in range(lenght):
+                for j in range(lenght):
+                    print("#", end="")
+                print("")
