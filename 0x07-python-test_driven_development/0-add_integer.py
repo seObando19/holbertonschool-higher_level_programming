c8987b9cb50e91809ebf04cbae45b863c800bca3
seObando19 <sebastiano-10@hotmail.com>
2020-09-27 04:10:38 +0000

    Updated task for fix with documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,15 @@ function that adds 2 integers.
 
 
 def add_integer(a, b=98):
+    """Add two numbers
+
+    Args:
+        a (int):first param
+        b (int):second param
+
+    Return:
+        int: add of two params
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
