f8b3c26b8525c52720820bf85764a31c3307805c
seObando19 <sebastiano-10@hotmail.com>
2020-09-29 01:55:31 +0000

    Completed task 6 and added test 6

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""
+Class Rectangle
+"""
+
+
+class Rectangle:
+    number_of_instances = 0
+    """
+        method initialized
+    """
+    def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    """
+        getter width
+    """
+    @property
+    def width(self):
+        return self.__width
+
+    """
+        setter width
+    """
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """
+        getter height
+    """
+    @property
+    def height(self):
+        return self.__height
+
+    """
+        setter height
+    """
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    """
+        method for return Area to rectangle
+    """
+    def area(self):
+        return self.width * self.height
+
+    """
+        method for return perimeter to rectangle
+    """
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    """
+        should print the rectangle with the character #
+    """
+    def __str__(self):
+        valPrint = ""
+        if self.width is 0 or self.height is 0:
+            return valPrint
+        else:
+            w = self.width
+            h = self.height
+            for i in range(h):
+                if i + 1 < h:
+                    valPrint += "#" * w + "\n"
+                else:
+                    valPrint += "#" * w
+        return valPrint
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
