5b01887e488f935a61501c7447a54b2075e4b219
seObando19 <sebastiano-10@hotmail.com>
2020-10-07 22:52:03 +0000

    Completed task 4 and added test 4

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Module append
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    function that appends a string at the end of a text file
+
+    Return:
+     returns the number of characters added
+    """
+    with open(filename, "a",encoding='utf8') as myFile:
+        return myFile.write(text)
