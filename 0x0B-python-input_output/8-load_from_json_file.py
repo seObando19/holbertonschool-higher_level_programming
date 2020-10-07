a61254b5610f69e21384cc3e054cd935e76f35a6
seObando19 <sebastiano-10@hotmail.com>
2020-10-07 23:50:03 +0000

    Completed task 8 and added test 8

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+import  json module
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+        function that creates an Object from a “JSON file”
+    """
+    with open(filename, encoding='utf8') as myFile:
+        json.load(myFile)
