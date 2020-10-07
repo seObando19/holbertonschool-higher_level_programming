68cea202d64ff19d7553821ccba9274bd3a8dff9
seObando19 <sebastiano-10@hotmail.com>
2020-10-07 23:13:55 +0000

    Updated v3 task 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -10,12 +10,12 @@ def read_lines(filename="", nb_lines=0):
      and prints it to stdout.
     """
     with open(filename, "r", encoding='utf8') as myFile:
-        if nb_lines <= 0 or nb_lines > len(myFile.readlines()):
+        if nb_lines <= 0:
             print(myFile.read(), end="")
         else:
             totLines = 0
             for line in myFile:
                 totLines += 1
-                print(myFile.readline(), end="")
+                print(line, end="")
                 if totLines == nb_lines:
                     break
