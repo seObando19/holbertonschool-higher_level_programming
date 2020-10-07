ccd2f6a0c39fc9608322e562d93702f7914530d6
seObando19 <sebastiano-10@hotmail.com>
2020-10-07 01:32:29 +0000

    Updated task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,6 +5,9 @@ Write an empty class BaseGeometry.
 
 
 class BaseGeometry(Exception):
+    """
+    Write an empty class BaseGeometry.
+    """
     def area(self):
         """
             Method that raises an Exception with
