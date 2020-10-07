3e17d95bad692c418ea1c20d3d77bd4e9771fed6
seObando19 <sebastiano-10@hotmail.com>
2020-10-07 03:20:55 +0000

    Completed task 11 and added test 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Class Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        Class Square
+    """
+    def __init__(self, size):
+        """
+            Inialization od Square class
+
+            Args:
+                size: size of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+            Implement area method
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+            Return descriptor of Square
+
+            [Square] <width>/<height>
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
