7898e9195e5f86b9d5310cf14d99627caec0eb55
seObando19 <sebastiano-10@hotmail.com>
2020-10-08 04:08:25 +0000

    Updated v2 task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -13,7 +13,7 @@ class Student:
         Inialization of class
         """
         self.first_name = first_name
-        self.Last_name = Last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
