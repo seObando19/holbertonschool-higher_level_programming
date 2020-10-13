c1c69d386ec8ef1545ed6a03a0b749f6bc439a35
seObando19 <sebastiano-10@hotmail.com>
2020-10-13 18:58:58 +0000

    Completed module base v1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+    Import module JSON
+"""
+
+
+import json
+
+
+class Base:
+    """
+        Class base of all the project
+
+        arguments:
+            __nb_objects(integer)
+        methods:
+            __init__()
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+            Base class initialization method
+
+            arguments:
+                id(integer)
+        """
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id =+ type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+            function that returns the JSON representation
+            of an object (string)
+
+            Args:
+            list_dictionaries (list): List of dictionaries.
+
+	    Returns:
+            [str]: Json representation.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+            Write the JSON string representation.
+
+            Args:
+             list_objs (JSON object): JSON representation of list_objs.
+        """
+        filename = cls.__name__ + ".json"
+        for value in list_objs:
+            list_dict = value.to_dictionary()
+        obj = cls.to_json_string(list_dict)
+        with open(filename, mode='w', encoding='utf-8') as myFile:
+            myFile.write(obj)
