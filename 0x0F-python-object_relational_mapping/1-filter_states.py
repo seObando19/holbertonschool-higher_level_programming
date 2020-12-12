9eeaa3fba737d1ac673dfbdabc886508f4e35490
seObando19 <sebastiano-10@hotmail.com>
2020-12-12 12:29:11 -0500

    Completed task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
                             passwd=password,
                             db=database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = substring()  ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'  ORDER BY id ASC")
 
     rows = cur.fetchall()
     for row in rows:
