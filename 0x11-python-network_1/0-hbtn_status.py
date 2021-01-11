557b586822c0337d1db89c5325af1a4c0861f2a6
seObando19 <sebastiano-10@hotmail.com>
2021-01-11 13:04:53 -0500

    Created new folder | added README file | added task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#Python script that fetches https://intranet.hbtn.io/status
+    #You must use the package urllib
+    #You are not allowed to import any packages other than urllib
+    #The body of the response must be displayed like the following example (tabulation before -)
+    #You must use a with statement
+from urllib import request
+
+if __name__ == "__main__":
+    URL = "https://intranet.hbtn.io/status"
+
+    with request.urlopen(URL) as route:
+        info = route.read()
+
+    print("Body response:")
+    print("\t- type:", type(info))
+    print("\t- content:", info)
+    print("\t- utf8 content:", info.decode("utf-8"))
