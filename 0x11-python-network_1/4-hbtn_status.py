be0e217e5080979063028c1373ecab8f1149567b
seObando19 <sebastiano-10@hotmail.com>
2021-01-11 18:27:15 -0500

    Updated task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,14 +7,12 @@ You are not allow to import packages other than requests
 The body of the response must be display like the following
 example (tabulation before -)
 """
-from urllib import request
+import requests
 
 if __name__ == "__main__":
     URL = "https://intranet.hbtn.io/status"
-
-    with request.urlopen(URL) as response:
-        res = response.read().decode("utf-8")
+    req = requests.get(URL)
 
     print("Body response:")
-    print("\t- type:", type(res))
-    print("\t- content:", res)
+    print("\t- type:", type(req.content.decode("utf-8")))
+    print("\t- content:", req.content.decode("utf-8"))
