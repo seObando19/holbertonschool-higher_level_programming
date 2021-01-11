0dbcf25fbebc48b67180271378a448b2494aa88c
seObando19 <sebastiano-10@hotmail.com>
2021-01-11 13:08:10 -0500

    Updated task 0 with solveded error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-#Python script that fetches https://intranet.hbtn.io/status
-    #You must use the package urllib
-    #You are not allowed to import any packages other than urllib
-    #The body of the response must be displayed like the following example (tabulation before -)
-    #You must use a with statement
+""" Python script that fetches https://intranet.hbtn.io/status
+    You must use the package urllib
+    You are not allowed to import any packages other than urllib
+    The body of the response must be displayed like the following
+    example (tabulation before -)
+    You must use a with statement
+"""
 from urllib import request
 
 if __name__ == "__main__":
