c14794ee3f08b6b005694220c36636278d918df3
seObando19 <sebastiano-10@hotmail.com>
2021-01-11 14:38:17 -0500

    Completed task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found in
+the header of the response.
+
+    You must use the packages urllib and sys
+    You are not allow to import packages other than urllib and sys
+    The value of this variable is different for each request
+    You don’t need to check arguments passed to the script (number or type)
+    You must use a with statement
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    URL = argv[1]
+
+    with urllib.request.urlopen(URL) as res:
+        print(res.headers['X-Request-Id'])
