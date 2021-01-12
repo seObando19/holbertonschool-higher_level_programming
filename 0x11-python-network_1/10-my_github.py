83088035e2839e399f24eb8a87c5960f9ac3b1c4
seObando19 <sebastiano-10@hotmail.com>
2021-01-12 10:37:27 -0500

    Updated task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,7 +4,8 @@ Python script that takes your Github credentials (username and password)
 and uses the Github API to display your id
 
     You must use Basic Authentication with a personal access token as
-    password to access to your information (only read:user permission is needed)
+    password to access to your information (only read:user permission
+    is needed)
     The first argument will be your username
     The second argument will be your password
     (in your case, a personal access token as password)
